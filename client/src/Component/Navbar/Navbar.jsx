5efe8980f878d4cf3ec7449d8ad9d517e37f0a4d
charlie <charliephung@gmail.com>
2018-12-17 19:51:15 +0200

    add node-sass and navbar

---
+++ client/src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.scss";
+
+function Navbar() {
+  return (
+    <nav className="nav-bar">
+      <ul className="nav-listt">lol profile</ul>
+      <ul className="nav-list">search</ul>
+    </nav>
+  );
+}
+
+export default Navbar;

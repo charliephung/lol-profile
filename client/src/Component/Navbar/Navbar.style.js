f2f643c5bcf517dc31012c79758e0d6ae036d0c3
charlie <charliephung@gmail.com>
2018-12-17 20:28:30 +0200

    add styled component

---
+++ client/src/Component/Navbar/Navbar.style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  background-image: linear-gradient(to right, #667eea, #764ba2);
+`;
+
+export const Container = styled.div`
+  display: flex;
+  margin: 0 auto;
+  width: 1000px;
+  height: 60px;
+  align-items: center;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  list-style: none;
+  margin-left: ${props => (props.floatLeft ? "auto" : "")}
+  margin-right: ${props => (props.floatRight ? "auto" : "")}
+  
+`;
+export const Item = styled.li`
+  color: white;
+  font-size: 26px;
+  padding: 0 20px;
+`;
+export const SearchBox = styled.input``;

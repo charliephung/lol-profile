c83880b58b1fb98b67778b516f5f258fae2e4306
charlie <charliephung@gmail.com>
2018-12-20 16:26:24 +0200

    setup redux

---
+++ client/src/constants/index.js
@@ -0,0 +1,1 @@
+export const GET_MATCHES_LIST = "GET_MATCHES_LIST";
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "App/App";
+import App from "Component/App/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const ReduxConnectedApp = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(ReduxConnectedApp, document.getElementById("root"));
 
 serviceWorker.unregister();

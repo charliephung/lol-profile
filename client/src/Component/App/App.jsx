bfda6e7b3aea75fc9b454dad865a9b515d969a85
charlie <charliephung@gmail.com>
2018-12-21 00:25:03 +0200

    add redux to App.js

--- client/src/Component/App/App.jsx
+++ client/src/Component/App/App.jsx
@@ -3,7 +3,11 @@ import Navbar from "Component/Navbar/Navbar";
 import Header from "Component/Header/Header";
 import Profile from "Component/Profile/Profile";
 import Overview from "Component/Overview/Overview";
+import MatchesListContainer from "Component/MatchesList/MatchesListContainer";
 import styled from "styled-components";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { getMatchesList } from "actions";
 import { GlobalStyle } from "./App.styles";
 
 const Container = styled.div`
@@ -18,7 +22,14 @@ const Flex = styled.div`
 
 // https://cdn.communitydragon.org/latest/champion/236/splash-art
 class App extends Component {
+  componentDidMount() {
+    this.props.getMatchesList(
+      "AL5QShkjgo7Za7A3WyTBLTvknQLUuPT6vd5ViYehdG1O_bI"
+    );
+  }
   render() {
+    const { matchesList } = this.props;
+
     return (
       <div className="app">
         <GlobalStyle />
@@ -29,10 +40,27 @@ class App extends Component {
             <Profile />
             <Overview />
           </Flex>
+          <div>
+            <MatchesListContainer matchesList={matchesList} />
+          </div>
         </Container>
       </div>
     );
   }
 }
 
-export default App;
+const mapState = state => ({
+  matchesList: state.matchesList
+});
+const mapDispatch = {
+  getMatchesList
+};
+
+const EnhancedApp = compose(
+  connect(
+    mapState,
+    mapDispatch
+  )
+);
+
+export default EnhancedApp(App);

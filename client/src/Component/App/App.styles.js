989f2507049ad2db211a2f62ee88afaf68d7b0de
charlie <charliephung@gmail.com>
2019-01-07 23:28:42 +0200

    add app styles

--- client/src/Component/App/App.styles.js
+++ client/src/Component/App/App.styles.js
@@ -1,9 +1,26 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+export const Container = styled.div`
+  width: 1000px;
+  margin: 0 auto;
+`;
+export const Flex = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex: 1;
+`;
+
+export const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: ${props => props.widthPercent}%;
+`;
 
 export const GlobalStyle = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
         font-family: 'PT Sans', sans-serif;
+        color: #868686;
     }
 `;

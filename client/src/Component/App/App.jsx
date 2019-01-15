90cc179b6c90f9cf27ed844249919c7647b2ad01
Charlie <phunghiep2709@gmail.com>
2019-01-15 15:20:02 +0200

    check settimeout

--- client/src/Component/App/App.jsx
+++ client/src/Component/App/App.jsx
@@ -26,63 +26,65 @@ import { GlobalStyle } from "./App.styles";
 // https://cdn.communitydragon.org/latest/champion/236/splash-art
 class App extends Component {
   componentDidMount() {
-    const {
-      matchesList,
-      summonerInfo,
-      rankInfo,
-      summonerMastery
-    } = localStorage;
-    if (true) {
-      this.props.actSetSummonerInfo(JSON.parse(summonerInfo));
-      this.props.actSetRankInfo(JSON.parse(rankInfo));
-      this.props.actSetMatchesList(JSON.parse(matchesList));
-      this.props.actSetSummonerMastery(JSON.parse(summonerMastery));
-    } else {
-      getSummonerByName("Black Rangerrr")
-        .then(info => {
-          this.props.actSetSummonerInfo(info);
-          getSummonerMasteryById(info.id).then(mastery =>
-            this.props.actSetSummonerMastery(mastery)
-          );
-          getRankInfoById(info.id).then(rankInfo =>
-            this.props.actSetRankInfo(rankInfo)
-          );
-          return info.accountId;
-        })
-        .then(accountId => getMatchesList(accountId))
-        .then(matchesList =>
-          matchesList.map(match => getMatchInfo(match.gameId))
-        )
-        .then(promises => Promise.all(promises))
-        .then(match => {
-          this.props.actSetMatchesList(match);
-        })
-        .then(() => {
-          const {
-            matchesList,
-            summonerInfo,
-            rankInfo,
-            summonerMastery
-          } = this.props;
-          localStorage.setItem("matchesList", JSON.stringify(matchesList));
-          localStorage.setItem("summonerInfo", JSON.stringify(summonerInfo));
-          localStorage.setItem("rankInfo", JSON.stringify(rankInfo));
-          localStorage.setItem(
-            "summonerMastery",
-            JSON.stringify(summonerMastery)
-          );
-        });
-    }
+    setTimeout(() => {
+      const {
+        matchesList,
+        summonerInfo,
+        rankInfo,
+        summonerMastery
+      } = localStorage;
+      if (true) {
+        this.props.actSetSummonerInfo(JSON.parse(summonerInfo));
+        this.props.actSetRankInfo(JSON.parse(rankInfo));
+        this.props.actSetMatchesList(JSON.parse(matchesList));
+        this.props.actSetSummonerMastery(JSON.parse(summonerMastery));
+      } else {
+        getSummonerByName("Black Rangerrr")
+          .then(info => {
+            this.props.actSetSummonerInfo(info);
+            getSummonerMasteryById(info.id).then(mastery =>
+              this.props.actSetSummonerMastery(mastery)
+            );
+            getRankInfoById(info.id).then(rankInfo =>
+              this.props.actSetRankInfo(rankInfo)
+            );
+            return info.accountId;
+          })
+          .then(accountId => getMatchesList(accountId))
+          .then(matchesList =>
+            matchesList.map(match => getMatchInfo(match.gameId))
+          )
+          .then(promises => Promise.all(promises))
+          .then(match => {
+            this.props.actSetMatchesList(match);
+          })
+          .then(() => {
+            const {
+              matchesList,
+              summonerInfo,
+              rankInfo,
+              summonerMastery
+            } = this.props;
+            localStorage.setItem("matchesList", JSON.stringify(matchesList));
+            localStorage.setItem("summonerInfo", JSON.stringify(summonerInfo));
+            localStorage.setItem("rankInfo", JSON.stringify(rankInfo));
+            localStorage.setItem(
+              "summonerMastery",
+              JSON.stringify(summonerMastery)
+            );
+          });
+      }
+    }, 3000);
   }
   render() {
-    const { matchesList, summonerInfo, rankInfo,summonerMastery } = this.props;
+    const { matchesList, summonerInfo, rankInfo, summonerMastery } = this.props;
 
     return (
       <div className="app">
         <GlobalStyle />
         <Navbar />
         <Container>
-          <Header />
+          <Header summonerInfo={summonerInfo} />
           <Flex>
             <FlexColumn widthPercent={40}>
               <RankContainer rankInfo={rankInfo} />
